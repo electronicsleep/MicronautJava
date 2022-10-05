44530aa6683b0919b549ad6a93963738a8e368b6
Chris <electronicsleep@gmail.com>
2022-10-04 19:06:03 -0700

    update-http-response

--- src/main/java/com/imgidea/micronaut_java/TopScores.java
+++ src/main/java/com/imgidea/micronaut_java/TopScores.java
@@ -1,10 +1,19 @@
 package com.imgidea.micronaut_java;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.annotation.Status;
+import io.micronaut.http.HttpStatus;
+
+import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.http.hateoas.Link;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Optional;
 import org.slf4j.Logger;
@@ -32,8 +41,8 @@ public class TopScores {
     }
 
     @Post("/top-score")
-    public String topScore(@Body String jsonBody) {
-            logger.info("Endpoint: /top-score-post jsonBody " + jsonBody);
+    public HttpResponse topScore(HttpRequest request, @Body String jsonBody) {
+            logger.info("Endpoint: /top-score post jsonBody " + jsonBody);
             ObjectMapper mapper = new ObjectMapper();
             ScoreData scoreData = new ScoreData();
         try {
@@ -41,12 +50,14 @@ public class TopScores {
             scoreData = mapper.readValue(jsonBody, ScoreData.class);
             //Convert from Data Object to Json
             String jsonString = mapper.writeValueAsString(scoreData);
-            logger.info("Endpoint: /top-score-post jsonString: " + jsonString);
+            logger.info("Endpoint: /top-score post jsonString: " + jsonString);
         } catch (Exception e) {
             e.printStackTrace();
+            JsonError error = new JsonError("UnprocessableEntity").link(Link.SELF, Link.of(request.getUri()));
+            return HttpResponse.<JsonError>unprocessableEntity().body(error);
         }
-        logger.info("Endpoint: /top-score-post scoreData.name " + scoreData.name);
-        logger.info("Endpoint: /top-score-post scoreData.score " + scoreData.score);
-        return jsonBody;
+        logger.info("Endpoint: /top-score post scoreData.name " + scoreData.name);
+        logger.info("Endpoint: /top-score post scoreData.score " + scoreData.score);
+        return HttpResponse.ok().body(jsonBody);
     }
 }
