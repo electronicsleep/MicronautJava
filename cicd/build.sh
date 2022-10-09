1e3963c609f83ce4dcf1fa61b4d354be97d6478e
Chris <electronicsleep@gmail.com>
2022-10-09 10:27:17 -0700

    update-build

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -ex
 ./gradlew clean build
 ls -lh ./build/libs/*
--- src/main/java/com/imgidea/micronaut_java/TopScores.java
+++ src/main/java/com/imgidea/micronaut_java/TopScores.java
@@ -6,10 +6,7 @@ import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.QueryValue;
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.http.annotation.Status;
-import io.micronaut.http.HttpStatus;
 
 import io.micronaut.http.hateoas.JsonError;
 import io.micronaut.http.hateoas.Link;
