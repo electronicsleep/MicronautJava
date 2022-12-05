45293ff5a4e9f9b59c69b6282fed1b3232930271
Chris Robertson <electronicsleep@gmail.com>
2022-12-04 16:35:41 -0800

    adding-curl-test

--- src/test/curl-tests.sh
+++ src/test/curl-tests.sh
@@ -9,4 +9,6 @@ echo "-"
 curl --fail -X POST http://0.0.0.0:8080/add-score -H 'Content-Type: application/json' -d '{"name":"chris", "score":"90"}'
 echo "-"
 curl --fail -X GET "http://0.0.0.0:8080/top-score"
+echo "-"
+curl --fail -X GET "http://0.0.0.0:8080/text-html/chris"
 echo -e "\ntests pass"
